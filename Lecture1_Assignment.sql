7136f36166e4bf511c7fc3b14619d06490cb2f00
Dana187 <63101057+Dana187@users.noreply.github.com>
2020-08-16 12:30:47 +1000

    add a file helloworld.md

---
+++ Lecture1_Assignment.sql
@@ -0,0 +1,160 @@
+# SQL
+# 1.1 Select all the products in product category 
+
+SELECT *
+From Products
+Where CategoryID = 1
+
+# 1.2 How many in total?
+
+SELECT COUNT(ProductID)
+From Products
+Where CategoryID = 1
+
+
+# 2.1 Select all the customers from France. 
+
+SELECT *
+From Customers
+Where Country = 'France'
+
+# 2.2 Out of those, how many are at Paris?
+
+SELECT Count(CustomerID)
+From Customers
+Where Country = 'France' and City = 'Paris'
+
+
+# 3. How many orders are placed during 1996-07-01 and 1996-07-31?
+
+SELECT Count(OrderID)
+From Orders
+Where orderdate between 1/7/1996 and 31/7/1996
+
+
+# 4. Which employee graduated from University of Washington?
+
+SELECT EmployeeID, LastName, FirstName
+From Employees
+Where Notes Like '%University of Washington%'
+
+
+# 5.1 How many orders does each product ID have? 
+
+SELECT ProductID, Count(OrderID) AS Num_of_Orders
+From OrderDetails
+Group by ProductID
+
+# 5.2 What are the total quantity for each product? 
+
+SELECT ProductID, SUM(Quantity) AS Total_QTY
+FROM OrderDetails
+Group by ProductID
+
+# 5.3 What is the average quantity per order per product?
+
+SELECT OrderID, ProductID, Avg(Quantity) as AVG_QTY
+FROM OrderDetails
+Group by OrderID, ProductID
+
+
+# 6.1 How many different products are in each order? 
+
+SELECT OrderID, Count(ProductID) AS Num_of_Product_Categories
+FROM OrderDetails
+Group by OrderID
+
+# 6.2 Which order has the most number of unique products? 不确定
+
+SELECT Distinct OrderID, Count(ProductID) as Product_Count
+FROM OrderDetails
+GROUP BY OrderID
+Order by Count(ProductID) desc
+
+
+# 7.1 Which products are sold in jars? 
+
+SELECT * 
+FROM Products
+Where Unit Like '%jars%'
+
+#7.2 What is the most expensive product that's sold in jars?
+
+SELECT * 
+FROM Products
+Where Unit Like '%jars%'
+Order by price desc
+
+
+# 8. What are the product names are included in order ID 10250? 
+
+
+SELECT Products.ProductName
+from Products
+Right JOIN OrderDetails 
+on Products.ProductID = OrderDetails.ProductID
+Where OrderDetails.OrderID = 10250
+
+#为什么下面full join无法运行？
+
+SELECT Products.ProductName
+FROM Products
+FULL JOIN OrderDetails 
+on Products.ProductID = OrderDetails.ProductID
+Where OrderDetails.OrderID = 10250
+
+
+# 9. What products are contained in category 'Dairy Products'? 
+
+SELECT Products.ProductName
+FROM Products
+Right JOIN Categories 
+on Products.CategoryID = Categories.CategoryID
+Where Categories.CategoryName = 'Dairy Products'
+
+#为什么下面full join无法运行？
+
+SELECT Products.ProductName
+FROM Products
+FULL JOIN Categories 
+on Products.CategoryID = Categories.CategoryID
+Where Categories.CategoryName = 'Dairy Products'
+
+
+# Join practice
+
+# 1. How many orders are made by Germany customers in total? 25
+
+SELECT count(Orders.OrderID) as Num_of_Germany_Customers
+FROM Customers
+Right JOIN Orders
+on Orders.CustomerID = Customers.CustomerID
+Where Customers.Country = 'Germany'
+
+#为什么用orders join customers表结果是27？有什么不同？应该怎么选？
+
+
+# 2. How many orders are made by Germany customers that's shipped by shipper 2 in total?
+
+
+
+# 3. Are there any prepared meats products are supplied by Tokyo Traders?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
